2022b178c174eb5813c048f46b27991a2b1c4ccc
Eric Lahoda <elahoda@gmail.com>
2023-12-16 17:29:51 -0500

    new buttons w colors

--- script.js
+++ script.js
@@ -2,76 +2,79 @@ const quoteContainer = document.getElementById("quote-container");
 const quoteText = document.getElementById("quote");
 const authorText = document.getElementById("author");
 const twitterBtn = document.getElementById("twitter");
+const facebookBtn = document.getElementById("facebook");
+const linkedinBtn = document.getElementById("linkedin");
+const smsBtn = document.getElementById("sms");
 const newQuoteBtn = document.getElementById("new-quote");
+
 const loader = document.getElementById("loader");
 
-let apiQuotes = [];
-
-// Show Loading
-function loading() {
+function showLoadingSpinner() {
   loader.hidden = false;
   quoteContainer.hidden = true;
 }
 
-// Hide Loading
-function complete() {
-  quoteContainer.hidden = false;
-  loader.hidden = true;
+function removeLoadingSpinner() {
+  if (!loader.hidden) {
+    quoteContainer.hidden = false;
+    loader.hidden = true;
+  }
 }
 
-function newQuote() {
-  loading();
-  //   Random quote selector from apiQuotes array
-  const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
-
-  //   Check if author field is blank and replace it with 'Unknown'
-  if (!quote.author) {
-    authorText.textContent = "Unknown";
-  } else {
-    authorText.textContent = quote.author;
-  }
-  authorText.textContent = quote.author;
-
-  //   Check quote length to determine styling
-  if (quote.text.length > 120) {
-    quoteText.classList.add("long-quote");
-  } else {
-    quoteText.classList.remove("long-quote");
-  }
-  //   Set Quote, Hide Loader
-  quoteText.textContent = quote.text;
-  complete();
-
-  // random local quote selector from local array
-  // const quote = localQuotes[Math.floor(Math.random() * localQuotes.length)];
-
-  console.log(quote);
-}
-
-// Get Quotes From API
-async function getQuotes() {
-  loading();
-  const apiUrl = "https://jacintodesign.github.io/quotes-api/data/quotes.json";
+// Get Quote From API
+async function getQuote() {
+  showLoadingSpinner();
+  const proxyUrl = "https://sheltered-castle-56551-bd0942956b4f.herokuapp.com/";
+  const apiUrl =
+    "http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json";
   try {
-    const response = await fetch(apiUrl);
-    apiQuotes = await response.json();
-    // pick a random quote
-    newQuote();
+    const response = await fetch(proxyUrl + apiUrl);
+    const data = await response.json();
+    console.log(data);
+    // If Author is blank, add 'Unknown'
+    if (data.quoteAuthor === "") {
+      authorText.innerText = "Unknown";
+    } else {
+      authorText.innerText = data.quoteAuthor;
+    }
+    // Reduce font size for long quotes
+    if (data.quoteText.length > 120) {
+      quoteText.classList.add("long-quote");
+    } else {
+      quoteText.classList.remove("long-quote");
+    }
+    quoteText.innerText = data.quoteText;
+    // Stop Loader, Show Quote
+    removeLoadingSpinner();
+    throw new Error("oops");
   } catch (error) {
-    // Catch Error Here
+    //getQuote();
+    console.log("whoops, no quote", error);
   }
 }
-// Tweet Quote
+
 function tweetQuote() {
-  const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+  const quote = quoteText.innerText;
+  const author = authorText.innerText;
+  const twitterUrl = `https://twitter.com/intent/tweet?text=${quote} - ${author}`;
   window.open(twitterUrl, "_blank");
 }
+
+function shareViaSMS() {
+  // Get the quote text
+  const quote = quoteText.innerText;
+  const author = authorText.innerText;
+  // Create an SMS link
+  const smsLink = `sms:?body=${encodeURIComponent(quote)} -${author}`;
+
+  // Open the SMS link
+  window.location.href = smsLink;
+}
+
 // Event Listeners
-newQuoteBtn.addEventListener("click", newQuote);
+newQuoteBtn.addEventListener("click", getQuote);
 twitterBtn.addEventListener("click", tweetQuote);
+smsBtn.addEventListener("click", shareViaSMS);
 
-// on Load
-getQuotes();
-
-// if not using async await
-// newQuote();
+// On Load
+getQuote();
